94dcdeb3939bd40f81edc9f98a8381b74157f3f3
XOZAN Aykut <hozanbesir21@gmail.com>
2024-03-09 15:43:57 +0000

    create env.py file and add database url

--- culinary/settings.py
+++ culinary/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qny@j!4^bgrz^q1&ekwia1i14z*iyn_7$6-l2fm7_+c5@q95^k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-xozanaykut-course-d3wltwqeuvp.ws-eu108.gitpod.io','.herokuapp.com']
 
