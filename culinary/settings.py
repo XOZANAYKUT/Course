cd29ac84b8e9b27041dc278c215e8512f9e22f93
XOZAN Aykut <hozanbesir21@gmail.com>
2024-03-09 15:54:10 +0000

    deployment settings

--- culinary/settings.py
+++ culinary/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qny@j!4^bgrz^q1&ekwia1i14z*iyn_7$6-l2fm7_+c5@q95^k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-xozanaykut-course-d3wltwqeuvp.ws-eu108.gitpod.io','.herokuapp.com']
 
@@ -78,14 +78,16 @@ WSGI_APPLICATION = 'culinary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
