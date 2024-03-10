6c7542347efd6c9cc5a207c3ec07558e4c9147a8
XOZAN Aykut <hozanbesir21@gmail.com>
2024-03-10 10:35:25 +0000

    Added default author value, duration and status choices to Course model

--- courses/models.py
+++ courses/models.py
@@ -10,23 +10,15 @@ STATUS_CHOICES = (
     (0, "Draft"),
     (1, "Published")
 )
-
-REVIEW_CHOICES = (
-    (1, "Excellent"),
-    (2, "Good"),
-    (3, "Average"),
-    (4, "Poor"),
-    (5, "Very Poor")
-)
-
 class Course(models.Model):
     title = models.CharField(max_length=200, unique=True, verbose_name="Title")
     slug = models.SlugField(max_length=200, unique=True, verbose_name="URL Tag")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="courses", verbose_name="Instructor")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="courses_posts", default=1
+    )  # Varsayılan olarak, kullanıcı kimliği 1 olan bir kullanıcıyı ata.
     content = models.TextField(verbose_name="Course Content")
     date = models.DateField(verbose_name="Course Date")
     duration = models.CharField(max_length=20, choices=DURATION_CHOICES, verbose_name="Course Duration")
-    review = models.IntegerField(choices=REVIEW_CHOICES, verbose_name="Course Rating")
     created_on = models.DateTimeField(auto_now_add=True, verbose_name="Creation Date")
     status = models.IntegerField(choices=STATUS_CHOICES, default=0, verbose_name="Status")
     excerpt = models.TextField(blank=True)
